66a01ac2a2aeb83c323db368ff6f99b521cd90cc
pegueraeu <peguera_eu@live.com>
2024-05-24 12:54:19 -0300

    feat: remove content metadata

--- app/components/video-player.tsx
+++ app/components/video-player.tsx
@@ -9,29 +9,15 @@ export default function VideoPlayer({content}: PlayerProps) {
 
   return (
     <>
-      {/* Content title */}
-      <div className='row'>
-        <div className='col-12 text-center'>
-          <h1>{`${content.title}`}</h1>
-          <h2>{`${content.subtitle}, ${content.release_year}`}</h2>
-        </div>
-      </div>
       {/* The Content itself */}
       <div className='row content-row'>
-        <div className='col-10 offset-1 text-center media-box'>
+        <div className='col-12 text-center media-box'>
           {/* The content's media */}
           <video
             id='video'
             autoPlay muted
             className='img-fluid'
             src={path} />
-        </div>
-      </div>
-      {/* Content metadata */}
-      <div className='row'>
-        <div className='col-12 text-center metadata'>
-          <h2>Artist: {content.author_name}</h2>
-          <h3>Technique: {content.technique}</h3>
         </div>
       </div>
     </>
