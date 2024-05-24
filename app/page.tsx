89633f8bb5bc61bb14ba4dbb4cc67e71c0deebbd
pegueraeu <peguera_eu@live.com>
2024-05-24 12:55:24 -0300

    feat: remove websocket layer feat: change layout to fullscreen content feat: add SSE support

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { ReactElement, useEffect, useState } from "react";
-import io from 'socket.io-client'
 import { Content } from "./helpers/types";
 import ImagePlayer from "./components/image-player";
 import VideoPlayer from "./components/video-player";
@@ -9,32 +8,35 @@ export default function Home() {
   const [currentContent, setCurrentContent] = useState<Content>();
 
   //Page Initial setup
-  useEffect(() => {
-    const socket = io('http://localhost:3000');
 
-    //Connect to websocket, read from it and set content
-    socket.on('connection', (raw) => {
-      raw.request = null;
-    });
-    socket.on('content', (message) => {
-      setCurrentContent(message);
-    });
+  useEffect(() => {
+    const sse = new EventSource(`//localhost:3000/contents/events`, {});
+    function getRealtimeData(data: Content) {
+      setCurrentContent(data);
+    }
+    sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
+    sse.onerror = () => {
+      sse.close();
+    }
+    return () => {
+      sse.close();
+    };
   });
 
   //Conditional renders
   let currentContentPlayer: ReactElement;
   if (currentContent?.kind === "IMAGE") {
     currentContentPlayer = <ImagePlayer content={currentContent} />;
-  } else if(currentContent?.kind === "VIDEO"){
-    currentContentPlayer = <VideoPlayer content={currentContent}/>;
+  } else if (currentContent?.kind === "VIDEO") {
+    currentContentPlayer = <VideoPlayer content={currentContent} />;
   } else {
     currentContentPlayer = <h1>Waiting for content from controller</h1>
   }
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <main className="col-6 offset-3 player text-center">
+    <div className="container-fluid fullscreen">
+      <div className="row align-items-center">
+        <main className="col-8 offset-2 player text-center align-self-center">
           {currentContentPlayer}
         </main>
       </div>
